4a94b5a1b943954794bd0abd8215737f66463b04
roggen <robin@seerig.de>
2022-05-23 13:54:03 +0200

    update base

--- App.py
+++ App.py
@@ -1,25 +1,22 @@
-import sys
-import os
+from sys import exit
 
 from src.interface.PredictionWindow import PredictionWindow
 from src.interface.SplashScreen import SplashScreen
 from src.tools.Animation import opacity
 
-from PyQt5.QtCore import (
+from PySide2.QtCore import (
     Qt,
 )
 
-from PyQt5.QtWidgets import (
+from PySide2.QtWidgets import (
     QApplication,
 )
 
 from src.tools.ColorTheme import ColorTheme
 
 # logging.basicConfig(format="%(message)s", level=logging.INFO)
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QApplication()
     app.setApplicationName("emotions are Real")
     
     color_theme = ColorTheme()
@@ -29,7 +26,7 @@ if __name__ == "__main__":
     window.setWindowOpacity(0)
         
     splash = SplashScreen('./img/load.gif', Qt.WindowStaysOnTopHint)
-    window.setWindowOpacity(0)
+    splash.setWindowOpacity(0)
     
     splash.show()
     opacity(splash, 0, 1, 0.001)
@@ -40,4 +37,4 @@ if __name__ == "__main__":
     
     window.loadLibaryFolder('D:\\Musik\\Genres\\Electronic\\DnB\\liquid')
     splash.finish(window)
-    sys.exit(app.exec())
+    exit(app.exec_())
